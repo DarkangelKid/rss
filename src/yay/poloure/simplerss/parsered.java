fa92ad3bfc6ba60b7126dbd7865d12e53e07ede4
poloure <poloure@gmail.com>
2013-07-04 02:32:38 +0100

    parser

--- src/yay/poloure/simplerss/parsered.java
+++ src/yay/poloure/simplerss/parsered.java
@@ -54,10 +54,12 @@ class parsered
 							to_file(file_name + ".content.txt", current_tag.substring(1, current_tag.length() - 1) + "|", true);
 							while(!(end_tag.contains(end[i])))
 							{
-								cont = read_string_to_next_char(reader, '<')
-									.replaceAll("[\n|]", "")
+								cont = read_string_to_next_char(reader, '<', false)
+									.replaceAll("\n", "")
+									.replace("|", "")
 									.replace("&amp;", "&")
-									.replaceAll("[\r(&nbsp;)]", " ")
+									.replace("&nbsp;", " ")
+									.replaceAll("\r", " ")
 									.replace("&lt;", "<")
 									.replace("&gt;", ">")
 									.replace("&quot;", "\"")
@@ -82,27 +84,30 @@ class parsered
 								else if(description_length < 512)
 									to_file(file_name + ".content.txt", cont, true);
 
-								end_tag = read_string_to_next_char(reader, '>');
+								end_tag = "<" + read_string_to_next_char(reader, '>', true);
 
 								if(!(end_tag.contains(end[i])))
 								{
 									if(end_tag.contains("<![CDATA["))
 									{
 										end_tag = end_tag
-										.replaceAll("\r", " ")
-										.replaceAll("[\n|(<![CDATA[)(]]>)]", "")
-										.replace("&amp;", "&")
-										.replace("&lt;", "<")
-										.replace("&gt;", ">")
-										.replaceAll("\\<.*?\\>", "")
-										.replace("&quot;", "\"")
-										.replace("&mdash;", "—")
-										.replace("&hellip;", "…")
-										.replace("&#8217;", "’")
-										.replace("&#8216;", "‘")
-										.replaceAll("\t", "&t&")
-										.replace("</p>", "&n&")
-										.replace("&rsquo;", "'");
+											.replaceAll("\r", " ")
+											.replaceAll("\n", "")
+											.replace("|", "")
+											.replace("<![CDATA[", "")
+											.replace("]]>", "")
+											.replace("&amp;", "&")
+											.replace("&lt;", "<")
+											.replace("&gt;", ">")
+											.replaceAll("\\<.*?\\>", "")
+											.replace("&quot;", "\"")
+											.replace("&mdash;", "—")
+											.replace("&hellip;", "…")
+											.replace("&#8217;", "’")
+											.replace("&#8216;", "‘")
+											.replaceAll("\t", "&t&")
+											.replace("</p>", "&n&")
+											.replace("&rsquo;", "'");
 
 										take = description_length;
 
@@ -127,7 +132,7 @@ class parsered
 		}
 	}
 
-	private String read_string_to_next_char(BufferedReader reader, char next)
+	private String read_string_to_next_char(BufferedReader reader, char next, Boolean keep_last_char)
 	{
 		char[] current = new char[1];
 		char[] buf = new char[8192];
@@ -142,7 +147,8 @@ class parsered
 			}
 			count++;
 		}
-		buf[count] = current[0];
+		if(keep_last_char)
+			buf[count] = current[0];
 		return (new String(buf)).trim();
 	}
 
@@ -181,36 +187,35 @@ class parsered
 		boolean found = false;
 		String tag = "";
 		int eof = 0;
-		while((!found)&&(eof != -1)){
-			int count = 0;
-			char[] current = new char[1];
-			char[] buffer = new char[1024];
-			while((current[0] != '<')&&(eof != -1))
-				eof = reader.read(current, 0, 1);
-
-			if(eof != -1)
+		while((!found)&&(eof != -1))
+		{
+			char current = '\0';
+			while(current != '<')
 			{
-				tag = read_string_to_next_char(reader, '>');
+				eof = reader.read();
+				if(eof == -1)
+					return "eof";
+				else
+					current = (char) eof;
+			}
+			
+			tag = "<" + read_string_to_next_char(reader, '>', true);
 
-				if(tag.contains("img src="))
-					to_file(file_name + ".content.dump.txt", tag.substring(tag.indexOf("src=\"") + 5, tag.indexOf("\"", tag.indexOf("src=\"") + 6)) + "\n", false);
+			if(tag.contains("img src="))
+				to_file(file_name + ".content.dump.txt", tag.substring(tag.indexOf("src=\"") + 5, tag.indexOf("\"", tag.indexOf("src=\"") + 6)) + "\n", false);
 
-				if((tag.contains("type=\"text/html\""))&&(tag.contains("href=\"")))
-					to_file(file_name + ".content.url.txt", tag.substring(tag.indexOf("href=\"") + 6, tag.indexOf("\"", tag.indexOf("href=\"") + 7)) + "\n", false);
-				else if((tag.contains("type=\'text/html\'"))&&(tag.contains("href=\'")))
-					to_file(file_name + ".content.url.txt", tag.substring(tag.indexOf("href=\'") + 6, tag.indexOf("\'", tag.indexOf("href=\'") + 7)) + "\n", false);
+			if((tag.contains("type=\"text/html\""))&&(tag.contains("href=\"")))
+				to_file(file_name + ".content.url.txt", tag.substring(tag.indexOf("href=\"") + 6, tag.indexOf("\"", tag.indexOf("href=\"") + 7)) + "\n", false);
+			else if((tag.contains("type=\'text/html\'"))&&(tag.contains("href=\'")))
+				to_file(file_name + ".content.url.txt", tag.substring(tag.indexOf("href=\'") + 6, tag.indexOf("\'", tag.indexOf("href=\'") + 7)) + "\n", false);
 
-				for(String type : types)
-				{
-					if(tag.contains(type))
-						found = true;
-				}
+			for(String type : types)
+			{
+				if(tag.contains(type))
+					found = true;
 			}
 		}
-		if(eof == -1)
-			return "eof";
-		else
-			return tag;
+		return tag;
 	}
 
 	private void to_file(String file_namer, String string, boolean append)
