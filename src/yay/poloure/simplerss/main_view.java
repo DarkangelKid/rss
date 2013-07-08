c045c973766b06a64dff68c339992e86b690996e
Currfyg <currfyg@gmail.com>
2013-07-09 00:45:51 +0100

    wank

--- src/yay/poloure/simplerss/main_view.java
+++ src/yay/poloure/simplerss/main_view.java
@@ -294,10 +294,9 @@ public class main_view extends Activity
 		super.onStop();
 		Intent intent = new Intent(this, service_update.class);
 		intent.putExtra("GROUP_NUMBER", "0");
-		PendingIntent pend_intent = PendingIntent.getActivity(this, 0, intent, 0);
+		PendingIntent pend_intent = PendingIntent.getService(this, 0, intent, 0);
 		AlarmManager alarm_refresh = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
 		alarm_refresh.setRepeating(AlarmManager.RTC_WAKEUP, 60000, 60000, pend_intent);
-		//startService(intent);
 	}
 
 	@Override
@@ -306,6 +305,10 @@ public class main_view extends Activity
 		super.onStart();
 		if(!refreshing)
 			set_refresh(check_service_running());
+		Intent intentstop = new Intent(this, service_update.class);
+		PendingIntent senderstop = PendingIntent.getService(this, 0, intentstop, 0);
+		AlarmManager alarm_manager = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
+		alarm_manager.cancel(senderstop);
 	}
 
 	public static Resources get_resources(){
