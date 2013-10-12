dd9e835e19ea843387c95ebd5aa9e810a857e9b8
poloure <poloure@gmail.com>
2013-10-12 13:56:14 +0100

    duh.

--- SimpleRss/build/source/buildConfig/debug/com/poloure/simplerss/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.poloure.simplerss;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
